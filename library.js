4955f21157732f007de9fc0fdc8c9af987284b93
Harry Nicholls <harry.m.nicholls@gmail.com>
2022-08-17 21:53:51 -0400

    Fixes 404 issue

--- library.js
+++ library.js
@@ -1,73 +1,81 @@
-'use strict';
+"use strict";
 
-const user = require.main.require('./src/user');
-const controllers = require('./lib/controllers');
-const auth = require('./lib/auth');
-const deauth = require('./lib/deauth');
+const user = require.main.require("./src/user");
+const controllers = require("./lib/controllers");
+const auth = require("./lib/auth");
+const deauth = require("./lib/deauth");
 const plugin = {};
 
-plugin.init = async params => {
+plugin.init = async (params) => {
 	const router = params.router;
 	const middleware = params.middleware;
 
-	router.get('/admin/plugins/sso/lit-protocol', middleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/sso/lit-protocol', controllers.renderAdminPage);
-	router.post('/deauth/lit-protocol', [middleware.requireUser, middleware.applyCSRF], controllers.deauth);
+	router.get("/admin/plugins/sso/lit-protocol", middleware.admin.buildHeader, controllers.renderAdminPage);
+	router.get("/api/admin/plugins/sso/lit-protocol", controllers.renderAdminPage);
+	router.get("/auth/lit-protocol/callback", controllers.renderSuccess);
+	router.post("/deauth/lit-protocol", [middleware.requireUser, middleware.applyCSRF], controllers.deauth);
 
 	auth.init();
 };
 
-plugin.addMenuItem = async header => {
+plugin.addMenuItem = async (header) => {
 	header.plugins.push({
-		route: '/plugins/sso/lit-protocol',
-		icon: 'fa-ethereum',
-		name: 'Lit Protocol',
+		route: "/plugins/sso/lit-protocol",
+		icon: "fa-ethereum",
+		name: "Lit Protocol",
 	});
 
 	return header;
 };
 
-plugin.filterAuthInit = async loginStrategies => {
+plugin.filterAuthInit = async (loginStrategies) => {
 	loginStrategies.push({
-		name: 'lit-protocol',
-		url: '/auth/lit-protocol',
-		urlMethod: 'post',
-		callbackURL: '/auth/lit-protocol/callback',
-		icon: 'fa-ethereum',
-		scope: ''
+		name: "lit-protocol",
+		url: "/auth/lit-protocol",
+		urlMethod: "post",
+		callbackURL: "/auth/lit-protocol/callback",
+		icon: "fa-ethereum",
+		scope: "",
 	});
 
 	return loginStrategies;
 };
 
-plugin.filterAuthList = async authList => {
+plugin.filterAuthList = async (authList) => {
 	const { uid, associations } = authList;
-	const address = await user.getUserField(uid, 'web3:address');
+	const address = await user.getUserField(uid, "web3:address");
 
 	if (address) {
 		associations.push({
 			associated: true,
 			url: `https://www.etherscan.io/address/${address}`,
-			deauthUrl: '#',
-			name: 'web3 address',
-			icon: 'fa-ethereum',
-			component: 'web3/disassociate',
+			deauthUrl: "#",
+			name: "web3 address",
+			icon: "fa-ethereum",
+			component: "web3/disassociate",
 		});
 	} else {
 		associations.push({
 			associated: false,
-			name: 'web3 address',
-			url: '#',
-			icon: 'fa-ethereum',
-			component: 'web3/associate',
+			name: "web3 address",
+			url: "#",
+			icon: "fa-ethereum",
+			component: "web3/associate",
 		});
 	}
 
 	return authList;
 };
 
-plugin.filterUserWhitelistFields = async data => {
-	data.whitelist.push('web3:address');
+plugin.filterAuthOptions = ({ req, opts }) => {
+	if (req.path === "/auth/lit-protocol") {
+		return { opts: Object.assign({}, opts, { successRedirect: "/auth/lit-protocol/callback", failureRedirect: "/", failureMessage: true }) };
+	}
+	return opts;
+};
+
+plugin.filterUserWhitelistFields = async (data) => {
+	data.whitelist.push("web3:address");
 	return data;
 };
 
